038c8276fbcde5cf15546c457acfe8d888cfd8cf
Natalia Popova <popovanatalia.dev@gmail.com>
2025-07-11 01:09:26 +0300

    slight fix

--- src/components/AccountsList.vue
+++ src/components/AccountsList.vue
@@ -3,7 +3,6 @@
     <a-typography-title :level="2">Учетные записи</a-typography-title>
     <a-button type="primary" :icon="h(PlusOutlined)" @click="store.addAccount"> Добавить </a-button>
   </PageHeader>
-
   <AccountForm v-for="acc in store.accounts" :key="acc.id" :account="acc" />
 </template>
 
@@ -15,13 +14,14 @@ import { useAccountsStore } from '@/stores/accounts.store';
 import AccountForm from './AccountForm.vue';
 
 const store = useAccountsStore();
-</script>
 
-<script lang="ts">
 const PageHeader = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: end;
   margin-bottom: 24px;
+  h2 {
+    margin-bottom: 0;
+  }
 `;
 </script>
